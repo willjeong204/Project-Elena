f7e7efeeacdcb74a0b908ed34c10a3580ed80eed
evansliang <evansliang95@gmail.com>
2017-12-10 05:38:20 -0500

    updated Google_Map_UI.java, accurately display starting and ending point

--- src/javaapplication7/Google_Map_UI.java
+++ src/javaapplication7/Google_Map_UI.java
@@ -1,41 +1,131 @@
 package javaapplication7;
 
-import com.teamdev.jxmaps.GeocoderRequest;
+import com.teamdev.jxmaps.ControlPosition;
 import com.teamdev.jxmaps.GeocoderCallback;
-import com.teamdev.jxmaps.Marker;
-import com.teamdev.jxmaps.MapViewOptions;
-import com.teamdev.jxmaps.InfoWindow;
-import com.teamdev.jxmaps.GeocoderStatus;
+import com.teamdev.jxmaps.GeocoderRequest;
 import com.teamdev.jxmaps.GeocoderResult;
+import com.teamdev.jxmaps.GeocoderStatus;
+import com.teamdev.jxmaps.InfoWindow;
+import com.teamdev.jxmaps.LatLng;
 import com.teamdev.jxmaps.Map;
+import com.teamdev.jxmaps.MapMouseEvent;
+import com.teamdev.jxmaps.MapOptions;
 import com.teamdev.jxmaps.MapReadyHandler;
 import com.teamdev.jxmaps.MapStatus;
+import com.teamdev.jxmaps.MapTypeControlOptions;
+import com.teamdev.jxmaps.Marker;
+import com.teamdev.jxmaps.MouseEvent;
 import com.teamdev.jxmaps.swing.MapView;
 
 public class Google_Map_UI extends MapView {
-    public Google_Map_UI(MapViewOptions options) {
-        super(options);
-        setOnMapReadyHandler(new MapReadyHandler() {
+
+	private static final String INITIAL_LOCATION = "Amherst";
+
+    public Google_Map_UI() {
+    		// Setting of a ready handler to MapView object. onMapReady will be called when map initialization is done and
+    		// the map object is ready to use. Current implementation of onMapReady customizes the map object.
+    		setOnMapReadyHandler(new MapReadyHandler() {
+    			@Override
+    			public void onMapReady(MapStatus status) {
+	            // Getting the associated map object
+	            final Map map = getMap();
+	            // Setting initial zoom value
+	            map.setZoom(7.0);
+	            // Creating a map options object
+	            MapOptions options = new MapOptions();
+	            // Creating a map type control options object
+	            MapTypeControlOptions controlOptions = new MapTypeControlOptions();
+	            // Changing position of the map type control
+	            controlOptions.setPosition(ControlPosition.TOP_RIGHT);
+	            // Setting map type control options
+	            options.setMapTypeControlOptions(controlOptions);
+	            // Setting map options
+	            map.setOptions(options);
+	
+//	            performGeocode(INITIAL_LOCATION,"Boston");
+	        }
+    		});
+    }
+    private void performGeocode(String src, String dest) {
+        // Getting the associated map object
+        final Map map = getMap();
+        // Creating a geocode request
+        GeocoderRequest requestsrc = new GeocoderRequest();
+        // Setting address to the geocode request
+        requestsrc.setAddress(src);
+
+        // Geocoding position by the entered address
+        getServices().getGeocoder().geocode(requestsrc, new GeocoderCallback(map) {
             @Override
-            public void onMapReady(MapStatus status) {
-                if (status == MapStatus.MAP_STATUS_OK) {
-                    final Map map = getMap();
-                    map.setZoom(5.0);
-                    GeocoderRequest request = new GeocoderRequest(map);
-                    request.setAddress("Amherst, MA");
-
-                    getServices().getGeocoder().geocode(request, new GeocoderCallback(map) {
+            public void onComplete(GeocoderResult[] results, GeocoderStatus status) {
+                // Checking operation status
+                if ((status == GeocoderStatus.OK) && (results.length > 0)) {
+                    // Getting the first result
+                    GeocoderResult result = results[0];
+                    // Getting a location of the result
+                    LatLng location = result.getGeometry().getLocation();
+                    // Setting the map center to result location
+                    map.setCenter(location);
+                    // Creating a marker object
+                    Marker marker = new Marker(map);
+                    // Setting position of the marker to the result location
+                    marker.setPosition(location);
+                    // Creating an information window
+                    InfoWindow infoWindow = new InfoWindow(map);
+                    // Putting the address and location to the content of the information window
+                    infoWindow.setContent("<b>" + result.getFormattedAddress() + "</b><br>" + location.toString());
+                    // Moving the information window to the result location
+                    infoWindow.setPosition(location);
+                    // Showing of the information window
+                    infoWindow.open(map, marker);
+                    
+                    // Adding event listener that intercepts clicking on marker
+                    marker.addEventListener("click", new MapMouseEvent() {
                         @Override
-                        public void onComplete(GeocoderResult[] result, GeocoderStatus status) {
-                            if (status == GeocoderStatus.OK) {
-                                map.setCenter(result[0].getGeometry().getLocation());
-                                Marker marker = new Marker(map);
-                                marker.setPosition(result[0].getGeometry().getLocation());
+                        public void onEvent(MouseEvent mouseEvent) {
+                            // Removing marker from the map
+                            marker.remove();
+                        }
+                    });
+                }
+            }
+        });
+        
+        GeocoderRequest requestdest = new GeocoderRequest();
+        // Setting address to the geocode request
+        requestdest.setAddress(dest);
 
-                                final InfoWindow window = new InfoWindow(map);
-                                window.setContent("Hello, World!");
-                                window.open(map, marker);
-                            }
+        // Geocoding position by the entered address
+        getServices().getGeocoder().geocode(requestdest, new GeocoderCallback(map) {
+            @Override
+            public void onComplete(GeocoderResult[] results, GeocoderStatus status) {
+                // Checking operation status
+                if ((status == GeocoderStatus.OK) && (results.length > 0)) {
+                    // Getting the first result
+                    GeocoderResult result = results[0];
+                    // Getting a location of the result
+                    LatLng location = result.getGeometry().getLocation();
+                    // Setting the map center to result location
+                    map.setCenter(location);
+                    // Creating a marker object
+                    Marker marker = new Marker(map);
+                    // Setting position of the marker to the result location
+                    marker.setPosition(location);
+                    // Creating an information window
+                    InfoWindow infoWindow = new InfoWindow(map);
+                    // Putting the address and location to the content of the information window
+                    infoWindow.setContent("<b>" + result.getFormattedAddress() + "</b><br>" + location.toString());
+                    // Moving the information window to the result location
+                    infoWindow.setPosition(location);
+                    // Showing of the information window
+                    infoWindow.open(map, marker);
+                    
+                    // Adding event listener that intercepts clicking on marker
+                    marker.addEventListener("click", new MapMouseEvent() {
+                        @Override
+                        public void onEvent(MouseEvent mouseEvent) {
+                            // Removing marker from the map
+                            marker.remove();
                         }
                     });
                 }
