1b38992b66a61f6330e9ca9db3d015db64022e0f
Akanksha Sharma <akankshasharma033@gmail.com>
2017-12-11 00:12:33 -0500

    made changes to model - added indexIDMap

--- src/javaapplication7/Model.java
+++ src/javaapplication7/Model.java
@@ -1,31 +1,33 @@
 package javaapplication7;
 
 import com.opencsv.CSVReader;
+import org.jdom2.JDOMException;
+import org.xml.sax.SAXException;
+
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import javaapplication7.OsmParsing;
-import javaapplication7.NodeObject;
-import org.jdom2.JDOMException;
-import org.xml.sax.SAXException;
 
-public class Model extends java.util.Observable {	
+public class Model extends java.util.Observable {
 	private String source;
 	private String destination;
-        static HashMap<String, ArrayList<String>> adjMatrix;
-        ArrayList<NodeObject> mapNodes;
+	static HashMap<String, ArrayList<String>> adjMatrix;
+	static HashMap<String,Integer> indexIDMap = new HashMap<>();
+	ArrayList<NodeObject> mapNodes;
+	ArrayList<NodeObject> routeNodes;
 
 	public Model() throws JDOMException, SAXException, IOException, Exception{
-         
-            adjMatrix = readCSV();
-            
-            OsmParsing osm = new OsmParsing();
-            mapNodes = osm.parser();
-            
-        }
 
-        
+		adjMatrix = readCSV();
+
+		OsmParsing osm = new OsmParsing();
+		mapNodes = osm.parser();
+		indexIDMap = osm.indexIDMap;
+
+	}
+
+
 	public void setSource(String src) {
 		source = src;
 	}
@@ -39,39 +41,39 @@ public class Model extends java.util.Observable {
 		return destination;
 	}
 
-        private static HashMap<String, ArrayList<String>> readCSV() {
+	private static HashMap<String, ArrayList<String>> readCSV() {
 		CSVReader csvReader = null;
 		HashMap<String,ArrayList<String>> adjMatrix = new HashMap<>();
-        try
-        {        	
-            csvReader = new CSVReader(new FileReader("C:\\Users\\shrut\\Documents\\SE\\Project\\Project-Elena\\src\\javaapplication7\\adjacency.csv"),',','"');
-            String[] adjList = null;
-            while((adjList = csvReader.readNext())!=null)
-            {
-            	ArrayList<String> lines = new ArrayList<String>();
-            	for(int i =1;i<adjList.length;i++){
-            		lines.add(adjList[i]);
-            	}
-            	adjMatrix.put(adjList[0], lines);
-            }
-        }
-        catch(Exception ee)
-        {
-            ee.printStackTrace();
-        }
-        finally
-        {
 		try
 		{
-			csvReader.close();
+			csvReader = new CSVReader(new FileReader("C:\\Users\\Akanksha Sharma\\Desktop\\cs520-proj\\adjacency.csv"),',','"');
+			String[] adjList = null;
+			while((adjList = csvReader.readNext())!=null)
+			{
+				ArrayList<String> lines = new ArrayList<String>();
+				for(int i =1;i<adjList.length;i++){
+					lines.add(adjList[i]);
+				}
+				adjMatrix.put(adjList[0], lines);
+			}
 		}
 		catch(Exception ee)
 		{
 			ee.printStackTrace();
 		}
-	}
-	return adjMatrix;
+		finally
+		{
+			try
+			{
+				csvReader.close();
+			}
+			catch(Exception ee)
+			{
+				ee.printStackTrace();
+			}
+		}
+		return adjMatrix;
 
 	}
-    
+
 }
