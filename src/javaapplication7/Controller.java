6e1bc9d19fb1b4d66970634dde0b1be2f9bdc519
Akanksha Sharma <akankshasharma033@gmail.com>
2017-12-13 17:53:32 -0500

    added Add-to-fav functionality, added an arrayList to store the names of favourite routes, created a method to write the route to the file(lat-long pair separated by ;)

--- src/javaapplication7/Controller.java
+++ src/javaapplication7/Controller.java
@@ -1,16 +1,15 @@
 package javaapplication7;
 
+import org.jdom2.JDOMException;
+import org.json.JSONException;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javaapplication7.FindRoute;
-import javax.xml.parsers.ParserConfigurationException;
-import org.jdom2.JDOMException;
-
-import org.json.JSONException;
-import org.xml.sax.SAXException;
 
 public class Controller implements java.awt.event.ActionListener{
 	Model model;
@@ -21,84 +20,95 @@ public class Controller implements java.awt.event.ActionListener{
 	//invoked when a button is pressed
 	public void actionPerformed(java.awt.event.ActionEvent e){
 		String action = e.getActionCommand();
-                
-		switch (action) {
-		case "MAX":
-			model.setisMax(true);
-			model.setisMin(false);
-			break;
-		case "MIN":
-			model.setisMax(false);
-			model.setisMin(true);
-			break;
-		case "CLEAR":
-			model.setSource("");
-			model.setDestination("");
-			view.clear();
-//			view.getMapView().getMap().get
-			//need to figure out how to clear the map.
-//			view.getMapView().getMap().dispose();
-			break;
-		case "GO":
-			//clear the map first then add more.	
-			model.setSource(view.getSrc());
-			model.setDestination(view.getDest());
-			view.getMapView().performGeocode(model.getSource(), model.getDestination());
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e2) {
-				// TODO Auto-generated catch block
-				e2.printStackTrace();
-			}
-			//call routing algorithm
-			try {
-				ArrayList<Integer> srcDst = new ArrayList<>();
-				srcDst = model.getNodeId(view.getMapView());
-                                FindRoute r = new FindRoute();
-                try {
-                    boolean minimize_elevation = false;
-                    if(model.getisMin() == true)
-                    {
-                        minimize_elevation = true;
-                    }
-                    if(model.getisMax()==true)
-                    {
-                        minimize_elevation = false;
-                    }
-                    
-        
-                    model.final_route = r.route(model.mapNodes.get(srcDst.get(0)), model.mapNodes.get(srcDst.get(1)), model.mapNodes, model.indexIDMap,model.adjMatrix,1.0f,model.max_elevation, minimize_elevation);
-                } catch (MalformedURLException ex) {
-                    Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (IOException ex) {
-                    Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (JDOMException ex) {
-                    Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (ParserConfigurationException ex) {
-                    Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (SAXException ex) {
-                    Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
-                }
-				
-			} catch (IOException | JSONException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-			
-			//store latlng in mode here
-			view.getMapView().drawRoute(view.getMapView().getMap(), model);
 
-			break;
-		case "ADDFAV":
-			//DOSomething
-			break;
+		switch (action) {
+			case "MAX":
+				model.setisMax(true);
+				model.setisMin(false);
+				break;
+			case "MIN":
+				model.setisMax(false);
+				model.setisMin(true);
+				break;
+			case "CLEAR":
+				model.setSource("");
+				model.setDestination("");
+				view.clear();
+//			view.getMapView().getMap().get
+				//need to figure out how to clear the map.
+//			view.getMapView().getMap().dispose();
+				break;
+			case "GO":
+				//clear the map first then add more.
+				model.setSource(view.getSrc());
+				model.setDestination(view.getDest());
+				view.getMapView().performGeocode(model.getSource(), model.getDestination());
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e2) {
+					// TODO Auto-generated catch block
+					e2.printStackTrace();
+				}
+				//call routing algorithm
+				try {
+					ArrayList<Integer> srcDst = new ArrayList<>();
+					srcDst = model.getNodeId(view.getMapView());
+					javaapplication7.FindRoute r = new javaapplication7.FindRoute();
+					try {
+						boolean minimize_elevation = false;
+						if(model.getisMin() == true)
+						{
+							minimize_elevation = true;
+						}
+						if(model.getisMax()==true)
+						{
+							minimize_elevation = false;
+						}
+
+
+						model.final_route = r.route(model.mapNodes.get(srcDst.get(0)), model.mapNodes.get(srcDst.get(1)), model.mapNodes, model.indexIDMap,model.adjMatrix,1.0f,model.max_elevation, minimize_elevation);
+					} catch (MalformedURLException ex) {
+						Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
+					} catch (IOException ex) {
+						Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
+					} catch (JDOMException ex) {
+						Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
+					} catch (ParserConfigurationException ex) {
+						Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
+					} catch (SAXException ex) {
+						Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
+					}
+
+				} catch (IOException | JSONException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+
+				//store latlng in mode here
+				view.getMapView().drawRoute(view.getMapView().getMap(), model);
+
+				break;
+			case "ADDFAV":
+				//String routeName= "abc"
+				String routeStr = "";
+				//model.fav_source_dest.add(routeName);
+
+				System.out.println(model.final_route);
+
+				for (String s : model.final_route)
+				{
+					routeStr += s + ";";
+				}
+
+				model.writeToFavsFile(routeStr);
+				break;
 		}
 
-	} 
+	}
 
 	public void addModel(Model m){
 		this.model = m;
-	} 
+	}
 
 	public void addView(View v){
 		this.view = v;
--- src/javaapplication7/Model.java
+++ src/javaapplication7/Model.java
@@ -1,174 +1,184 @@
 package javaapplication7;
 
 import com.opencsv.CSVReader;
-
 import org.jdom2.JDOMException;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.xml.sax.SAXException;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class Model extends java.util.Observable {
-	private String source;
-	private String destination;
-	private boolean isMax, isMin;
-	static HashMap<String, ArrayList<String>> adjMatrix;
-	static HashMap<String,Integer> indexIDMap = new HashMap<>();
-	ArrayList<NodeObject> mapNodes;
-	ArrayList<NodeObject> routeNodes;
-        float max_elevation;
-        ArrayList<String> final_route = new ArrayList<String>();
-        
-	public Model() throws JDOMException, SAXException, IOException, Exception{
+    private String source;
+    private String destination;
+    private boolean isMax, isMin;
+    static HashMap<String, ArrayList<String>> adjMatrix;
+    static HashMap<String,Integer> indexIDMap = new HashMap<>();
+    ArrayList<NodeObject> mapNodes;
+    ArrayList<NodeObject> routeNodes;
+    float max_elevation;
+    ArrayList<String> final_route = new ArrayList<String>();
+    ArrayList<String> fav_route_mapping = new ArrayList<String>();
 
-		adjMatrix = readCSV();
+    //PrintWriter pw = new PrintWriter(new FileOutputStream(
+    //        new File("javaapplication7\\fav_routes.csv"), true));
+    File file = new File ("javaapplication7\\fav_routes.csv");
+    PrintWriter pw = new PrintWriter ("fav_routes.csv");
+    StringBuilder sb = new StringBuilder();
+    ArrayList<String> fav_source_dest = new ArrayList<String>();
 
-		OsmParsing osm = new OsmParsing();
-		mapNodes = osm.parser();
-		indexIDMap = osm.indexIDMap;
-                max_elevation = osm.max_elevation;
+    public Model() throws JDOMException, SAXException, IOException, Exception{
 
-	}
+        adjMatrix = readCSV();
 
-	public boolean getisMax() {
-		return isMax;
-	}
-	public boolean getisMin() {
-		return isMin;
-	}
-	public void setisMax(boolean value) {
-		isMax = value;
-	}
-	public void setisMin(boolean value) {
-		isMin = value;
-	}
-	public void setSource(String src) {
-		source = src;
-	}
-	public String getSource() {
-		return source;
-	}
-	public void setDestination(String dest) {
-		destination = dest;
-	}
-	public String getDestination() {
-		return destination;
-	}
+        OsmParsing osm = new OsmParsing();
+        mapNodes = osm.parser();
+        indexIDMap = osm.indexIDMap;
+        max_elevation = osm.max_elevation;
 
-	public ArrayList<Integer> getNodeId(Google_Map_UI mapView) throws IOException, JSONException{
-		//Google_Map_UI ui =new Google_Map_UI();
-		double srcLat = mapView.getsrcLatLng().getLat();//
-		double srcLng = mapView.getsrcLatLng().getLng();
-		System.out.println(srcLat);
-		System.out.println(srcLng);
-		double dstLat = mapView.destLatLng.getLat();
-		double dstLng = mapView.destLatLng.getLng() ;
-		System.out.println(dstLat);
-		System.out.println(dstLng);
-		ArrayList<Integer> srcDestIndex = new ArrayList<>();
-		int srcIndex = getId(srcLat,srcLng);
-		int dstIndex = getId(dstLat,dstLng);
-		srcDestIndex.add(srcIndex);
-		srcDestIndex.add(dstIndex);
-		return srcDestIndex;
-		
-	}
-	private int getId(Double lat,Double lng) throws IOException, JSONException {
-		// TODO Auto-generated method stub
-		//http://router.project-osrm.org/nearest/v1/driving/13.388860,52.517037?number=3&bearings=0,20
-		
-		List<String> testArray = new ArrayList<String>();
+    }
+
+    public boolean getisMax() {
+        return isMax;
+    }
+    public boolean getisMin() {
+        return isMin;
+    }
+    public void setisMax(boolean value) {
+        isMax = value;
+    }
+    public void setisMin(boolean value) {
+        isMin = value;
+    }
+    public void setSource(String src) {
+        source = src;
+    }
+    public String getSource() {
+        return source;
+    }
+    public void setDestination(String dest) {
+        destination = dest;
+    }
+    public String getDestination() {
+        return destination;
+    }
+
+    public ArrayList<Integer> getNodeId(Google_Map_UI mapView) throws IOException, JSONException{
+        //Google_Map_UI ui =new Google_Map_UI();
+        double srcLat = mapView.getsrcLatLng().getLat();//
+        double srcLng = mapView.getsrcLatLng().getLng();
+        System.out.println(srcLat);
+        System.out.println(srcLng);
+        double dstLat = mapView.destLatLng.getLat();
+        double dstLng = mapView.destLatLng.getLng() ;
+        System.out.println(dstLat);
+        System.out.println(dstLng);
+        ArrayList<Integer> srcDestIndex = new ArrayList<>();
+        int srcIndex = getId(srcLat,srcLng);
+        int dstIndex = getId(dstLat,dstLng);
+        srcDestIndex.add(srcIndex);
+        srcDestIndex.add(dstIndex);
+        return srcDestIndex;
+
+    }
+    private int getId(Double lat,Double lng) throws IOException, JSONException {
+        // TODO Auto-generated method stub
+        //http://router.project-osrm.org/nearest/v1/driving/13.388860,52.517037?number=3&bearings=0,20
+
+        List<String> testArray = new ArrayList<String>();
         HttpURLConnection connection = null;
-		 StringBuilder result = new StringBuilder();
-	        String base_url = "http://router.project-osrm.org/nearest/v1/foot/";
-	        String location = lng + "," +lat;
-	        String number = "?number=3";
-	        String bearings = "&bearings=0,20";
-	        String full_url = base_url+location+number+bearings;
-	        System.out.println(full_url);
-	        URL url = new URL(full_url);
-	        //URL url = new URL("https://maps.googleapis.com/maps/api/directions/json?origin=hadley,MA,US&destination=Umass+amherst,MA,US&mode=bicycling&alternatives=true&key=AIzaSyDbJBCyTJBUmRSrlAOfzc4AbdjBqZgoSRU");
-	        connection = (HttpURLConnection) url.openConnection();
-	        connection.setRequestMethod("GET");
-	        
-	        BufferedReader rd = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-	        String line;
-	        
-	        while ((line = rd.readLine()) != null) 
-	        {
-	        	System.out.println(line);       
-	                result.append(line);
-	        }
-	      rd.close();
-	      
-	      HashMap<String,String> srcDest = new HashMap<>();
-	      JSONObject jsonObject = new JSONObject(result.toString());
-	      
-	      JSONArray resultsArray=jsonObject.getJSONArray("waypoints");
-	      String x=null;
-	      for ( int i=0;i<resultsArray.length();i++){
-	    	  JSONObject routeObj=(JSONObject)resultsArray.get(i);
-	          JSONArray legs = routeObj.getJSONArray("nodes");
-	          
-	          for ( int j=0;j<legs.length();j++){
-	              String nodeID=(String)legs.get(j).toString();
-	                  System.out.println("here"+nodeID);
-	                  if (indexIDMap.containsKey(nodeID)){
-	                  int index = indexIDMap.get(nodeID);
-	                  System.out.println("index"+index);
-	          				return index;
-	                  }
-	          			}
-	                  }
-	          
-		return -1;
-	}
+        StringBuilder result = new StringBuilder();
+        String base_url = "http://router.project-osrm.org/nearest/v1/foot/";
+        String location = lng + "," +lat;
+        String number = "?number=3";
+        String bearings = "&bearings=0,20";
+        String full_url = base_url+location+number+bearings;
+        System.out.println(full_url);
+        URL url = new URL(full_url);
+        //URL url = new URL("https://maps.googleapis.com/maps/api/directions/json?origin=hadley,MA,US&destination=Umass+amherst,MA,US&mode=bicycling&alternatives=true&key=AIzaSyDbJBCyTJBUmRSrlAOfzc4AbdjBqZgoSRU");
+        connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+
+        BufferedReader rd = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String line;
+
+        while ((line = rd.readLine()) != null)
+        {
+            System.out.println(line);
+            result.append(line);
+        }
+        rd.close();
+
+        HashMap<String,String> srcDest = new HashMap<>();
+        JSONObject jsonObject = new JSONObject(result.toString());
+
+        JSONArray resultsArray=jsonObject.getJSONArray("waypoints");
+        String x=null;
+        for ( int i=0;i<resultsArray.length();i++){
+            JSONObject routeObj=(JSONObject)resultsArray.get(i);
+            JSONArray legs = routeObj.getJSONArray("nodes");
+
+            for ( int j=0;j<legs.length();j++){
+                String nodeID=(String)legs.get(j).toString();
+                System.out.println("here"+nodeID);
+                if (indexIDMap.containsKey(nodeID)){
+                    int index = indexIDMap.get(nodeID);
+                    System.out.println("index"+index);
+                    return index;
+                }
+            }
+        }
+
+        return -1;
+    }
 
 
-	private static HashMap<String, ArrayList<String>> readCSV() {
-		CSVReader csvReader = null;
-		HashMap<String,ArrayList<String>> adjMatrix = new HashMap<>();
-		try
-		{
-			csvReader = new CSVReader(new FileReader("C:\\Users\\shrut\\Documents\\SE\\Project\\Project-Elena\\adjacency.csv"),',','"');
-			String[] adjList = null;
-			while((adjList = csvReader.readNext())!=null)
-			{
-				ArrayList<String> lines = new ArrayList<String>();
-				for(int i =1;i<adjList.length;i++){
-					lines.add(adjList[i]);
-				}
-				adjMatrix.put(adjList[0], lines);
-			}
-		}
-		catch(Exception ee)
-		{
-			ee.printStackTrace();
-		}
-		finally
-		{
-			try
-			{
-				csvReader.close();
-			}
-			catch(Exception ee)
-			{
-				ee.printStackTrace();
-			}
-		}
-		return adjMatrix;
+    private static HashMap<String, ArrayList<String>> readCSV() {
+        CSVReader csvReader = null;
+        HashMap<String,ArrayList<String>> adjMatrix = new HashMap<>();
+        try
+        {
+            csvReader = new CSVReader(new FileReader("C:\\Users\\Akanksha Sharma\\IdeaProjects\\projectElena-Local\\src\\javaapplication7\\adjacency.csv"),',','"');
+            String[] adjList = null;
+            while((adjList = csvReader.readNext())!=null)
+            {
+                ArrayList<String> lines = new ArrayList<String>();
+                for(int i =1;i<adjList.length;i++){
+                    lines.add(adjList[i]);
+                }
+                adjMatrix.put(adjList[0], lines);
+            }
+        }
+        catch(Exception ee)
+        {
+            ee.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                csvReader.close();
+            }
+            catch(Exception ee)
+            {
+                ee.printStackTrace();
+            }
+        }
+        return adjMatrix;
 
-	}
+    }
+
+    public void writeToFavsFile(String favRouteStr){
+        sb.append(favRouteStr+"\n");
+        pw.write(sb.toString());
+        sb.delete(0, sb.length());
+        pw.flush();
+    }
 
 }
